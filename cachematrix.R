aca266376a9b61b81f50bb93d822d54810ba11cd
chrlsjsph460 <chrlsjsph460@gmail.com>
2014-12-21 15:24:58 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Create a list of functions that set,get,setInverse,getInverse that do as names say 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 makeCacheMatrix <- function(x = matrix(nrow=0,ncol=0)){
-    inverse <- NULL
+    inverse <- NULL #set inv to null. its changed if you setInv
     set <- function(y){
-        x <<- y
-        inverse <<- NULL
+        x <<- y #change value of x to y and store in parent env
+        inverse <<- NULL #set inverse to null and store in parent
     }
     get <- function() x
-    setInverse <- function(xInv) inverse <<- xInv
+    setInverse <- function(xInv) inverse <<- xInv #set new inverse to xInv and store in parent 
     getInverse <- function() inverse
     list(set = set, get = get, 
          setInverse = setInverse,
@@ -22,13 +18,14 @@ makeCacheMatrix <- function(x = matrix(nrow=0,ncol=0)){
 }
 
 ## Write a short comment describing this function
-
+#cacheSolve returns the inverse of the matrix object from makeCacheMatrix. 
+#if the inverse is cached already we don't re-compute.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
-## comment later
 cacheSolve <- function(matrix, ...) {
-    inverse <- matrix$getInverse()
+    inverse <- matrix$getInverse() #gets inverse from matrix object, 
+    #if null we compute and store in parent envif not we use cached result 
     if(!is.null(inverse)) {
         message("getting cached data")
         return(inverse)
@@ -38,6 +35,6 @@ cacheSolve <- function(matrix, ...) {
         data <- matrix$get()
         inverse <- solve(data,...)
     }
-    matrix$setInverse(inverse)
+    matrix$setInverse(inverse) #store result in parent env
     inverse
 }
